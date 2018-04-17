e61f2b58b608699b1f3301c6917d1e157032a0c2
Yakup Karahan <yakup@karahan.de>
2018-04-17 14:13:34 +0200

    added SvgPathMarker for Scatterseries

--- src/lib/series/SplitMarker.js
+++ src/lib/series/SplitMarker.js
@@ -46,7 +46,7 @@ Split.defaultProps = {
 	strokeWidth: 1,
 	opacity: 0.5,
 	fill: "#4682B4",
-	className: "react-stockcharts-marker-rect",
+	className: "react-stockcharts-marker-split",
 };
 Split.drawOnCanvas = (props, point, ctx) => {
 	const { stroke, fill, opacity, strokeWidth } = props;
